785015d4fc18e564ad2acabce3c98775f4971fc3
Karoluks <karolis.malisauskas@gmail.com>
2018-02-27 00:11:09 +0200

    Cleaning up

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import http from "http"
-import app from "./app"
-
-const PORT = process.env.PORT  || 3000
-const server = http.createServer(app)
-server.listen(PORT, () => {
-    console.log(`<<<<Server is running on port: ${PORT}`)
-})
